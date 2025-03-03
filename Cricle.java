4fa0d743d57c4890e0e00dc3195a748637f5a672
vishalcode2023 <151914635+vishalcode2023@users.noreply.github.com>
2025-03-03 15:36:35 +0530

    Create Cricle.java

---
+++ Cricle.java
@@ -0,0 +1,7 @@
+public class Circle {
+    public static void main(String[] args) {
+        double radius = 5.0;  
+        double area = Math.PI * Math.pow(radius, 2); 
+        System.out.println("The area of the circle is: " + area);
+    }
+}
